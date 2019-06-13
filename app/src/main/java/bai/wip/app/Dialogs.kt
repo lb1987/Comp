9fff372a4074af97cc2ad5d62b94d0635369ed4c
lib <lib1987@126.com>
2019-06-13 22:39:53 +0800

    @task: unrelated dialog in bottom

--- app/src/main/java/bai/wip/app/Dialogs.kt
+++ app/src/main/java/bai/wip/app/Dialogs.kt
@@ -14,12 +14,10 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun Activity.alert(
-    msg: String = "警告",
-    layout: Int = R.layout.alert
+        msg: String = "警告",
+        layout: Int = R.layout.alert
 ) {
-    dialog(
-        layout
-    ) { _, view ->
+    dialog(layout) { _, view ->
         with(view) {
             msgTV.text = msg
         }
@@ -28,16 +26,16 @@ fun Activity.alert(
 
 @SuppressLint("SimpleDateFormat")
 fun Activity.dateBottomDialog(
-    title: String,
-    current: Calendar = Calendar.getInstance(),
-    start: Calendar? = null,
-    end: Calendar? = null,
-    format: String = "yyyy-MM-dd",
-    callback: (date: String) -> Unit
+        title: String,
+        current: Calendar = Calendar.getInstance(),
+        start: Calendar? = null,
+        end: Calendar? = null,
+        format: String = "yyyy-MM-dd",
+        callback: (date: String) -> Unit
 ) {
     dialog(
-        layout = R.layout.date_bottom_dialog,
-        isBottom = true
+            layout = R.layout.date_bottom_dialog,
+            isBottom = true
     ) { dialog, view ->
         val sdf = SimpleDateFormat(format)
         var result = sdf.format(current.time)
@@ -59,16 +57,16 @@ fun Activity.dateBottomDialog(
     }
 }
 
-fun <T> Activity.optionBottomDailog(
-    title: String,
-    xOptions: List<T>,
-    yOptions: List<List<T>>?,
-    zOptions: List<List<List<T>>>?,
-    callback: (x: T, y: T?, z: T?) -> Unit
+fun <T> Activity.optionRelatedDialog(
+        title: String,
+        xOptions: List<T>,
+        yOptions: List<List<T>>?,
+        zOptions: List<List<List<T>>>?,
+        callback: (x: T, y: T?, z: T?) -> Unit
 ) {
     dialog(
-        layout = R.layout.option_bottom_dialog,
-        isBottom = true
+            layout = R.layout.option_bottom_dialog,
+            isBottom = true
     ) { dialog, view ->
         var xResult: T = xOptions[0]
         var yResult: T? = yOptions?.get(0)?.get(0)
@@ -76,11 +74,11 @@ fun <T> Activity.optionBottomDailog(
         with(view) {
             titleTV.text = title
             val adapter =
-                OptionPicker.RelatedAdapter(xOptions, yOptions, zOptions) { x: T, y: T?, z: T? ->
-                    xResult = x
-                    yResult = y
-                    zResult = z
-                }
+                    OptionPicker.RelatedAdapter(xOptions, yOptions, zOptions) { x: T, y: T?, z: T? ->
+                        xResult = x
+                        yResult = y
+                        zResult = z
+                    }
             optionPK.setRelatedAdapter(adapter)
             sureBTN.setOnClickListener {
                 callback(xResult, yResult, zResult)
@@ -91,5 +89,40 @@ fun <T> Activity.optionBottomDailog(
                 dialog.dismiss()
             }
         }
+    }
+}
+
+fun <T> Activity.optionUnRelatedDialog(
+        title: String,
+        xOption: List<T>,
+        yOption: List<T>?,
+        zOption: List<T>?,
+        callback: (x: T, y: T?, z: T?) -> Unit
+) {
+    dialog(
+            layout = R.layout.option_bottom_dialog,
+            isBottom = true
+    ) { dialog, view ->
+        var xResult = xOption[0]
+        var yResult = yOption?.get(0)
+        var zResult = zOption?.get(0)
+        with(view) {
+            titleTV.text = title
+            val adapter = OptionPicker.UnRelatedAdapter(xOption, yOption, zOption) { x, y, z ->
+                xResult = x
+                yResult = y
+                zResult = z
+            }
+            optionPK.setUnrelatedAdapter(adapter)
+            sureBTN.setOnClickListener {
+                callback(xResult, yResult, zResult)
+                dialog.dismiss()
+            }
+
+            cancelBTN.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
     }
 }
