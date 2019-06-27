a5b081bb89885aad1384da097fbddb08ec6dd243
lib <lib1987@126.com>
2019-06-27 22:33:58 +0800

    @task: add setListener()

--- radio/src/main/java/bai/wip/radio/Radio.kt
+++ radio/src/main/java/bai/wip/radio/Radio.kt
@@ -11,24 +11,58 @@ import kotlinx.android.synthetic.main.radio.view.*
 import java.util.zip.Inflater
 
 class Radio @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
+    private var mCurrentClickListener = -1
+    private var mUpperClickListener: (() -> Unit)? = null
+    private var mMiddleClickListener: (() -> Unit)? = null
+    private var mUnderClickListener: (() -> Unit)? = null
+
     init {
         View.inflate(context, R.layout.radio, this)
         upperIV.setOnClickListener {
-            // 点击启动动画
+            mCurrentClickListener = 0
+            radio.transitionToStart()
         }
-        middleIV.setOnClickListener { }
-        underIV.setOnClickListener { }
+        middleIV.setOnClickListener {
+            mCurrentClickListener = 1
+            radio.transitionToStart()
+        }
+        underIV.setOnClickListener {
+            mCurrentClickListener = 2
+            radio.transitionToStart()
+        }
 
         radio.setTransitionListener(object : SimpleTransitionListener() {
             override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                when (mCurrentClickListener) {
+                    0 -> {
+                        mUpperClickListener?.invoke()
+                    }
 
+                    1 -> {
+                        mMiddleClickListener?.invoke()
+                    }
+
+                    2 -> {
+                        mUnderClickListener?.invoke()
+                    }
+                }
             }
-
         })
     }
+
+    fun setListener(
+        upperClickListener: (() -> Unit)? = null,
+        middleClickListener: (() -> Unit)? = null,
+        underClickListener: (() -> Unit)? = null
+    ) {
+        mUpperClickListener = upperClickListener
+        mMiddleClickListener = middleClickListener
+        mUnderClickListener = underClickListener
+    }
+
 
 }
