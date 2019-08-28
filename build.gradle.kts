656391d13dcde59fe5b569879fb6256930c225d6
lib <lib1987@126.com>
2019-08-28 23:56:18 +0800

    @task: update gradle version to 3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath("com.android.tools.build:gradle:3.5.0")
         //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
         classpath(kotlin("gradle-plugin", version = "1.3.31"))
         
